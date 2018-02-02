92c4be225b5a86cfc179e1019fa0271ba62470d1
Matthew Drake <mdrake@mediadrake.com>
2018-02-02 10:13:01 -0800

    instance example

--- src/examples/HowlinRedux.js
+++ src/examples/HowlinRedux.js
@@ -6,21 +6,30 @@ import logger from 'redux-logger';
 import reducer, { restock } from './redux';
 import HowlinMenuRedux from './redux-components/HowlinMenuRedux';
 
-const store = createStore(reducer, {}, applyMiddleware(logger));
-
-const HowlinRedux = () => (
-  <Provider store={store}>
-    <div>
-      <HowlinMenuRedux />
-      <button
-        type="button"
-        style={{padding: 16, width: '100%'}}
-        onClick={() => store.dispatch(restock())}
-      >
-        Restock!
-      </button>
-    </div>
-  </Provider>
-);
+class HowlinRedux extends React.Component {
+  componentWillMount() {
+    const store = createStore(reducer, {}, applyMiddleware(logger));
+    this.setState({
+      store,
+    });
+  }
+  render() {
+    const { store } = this.state;
+    return (
+      <Provider store={store}>
+        <div>
+          <HowlinMenuRedux />
+          <button
+            type="button"
+            style={{padding: 16, width: '100%'}}
+            onClick={() => store.dispatch(restock())}
+          >
+            Restock!
+          </button>
+        </div>
+      </Provider>
+    );
+  }
+}
 
 export default HowlinRedux;
